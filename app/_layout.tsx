533d706aadd12dfc481373eab1240375077a42dd
Hafiz Rayan Kurniawan <fiz@hrk.my.id>
2025-03-17 17:47:57 +0700

    testing prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
     InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
   });
-
+ 
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
