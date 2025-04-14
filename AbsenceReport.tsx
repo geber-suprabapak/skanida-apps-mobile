0f4c6c75d0074f5e905bfe011294769568f7f782
Hafiz Rayan Kurniawan <fiz@hrk.my.id>
2025-04-14 15:18:41 +0700

    remove unused code

--- AbsenceReport.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const AbsenceReport = () => {
-  return <Text>Absence Report Page</Text>;
-};
-
-export default AbsenceReport;
--- index.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from 'react-native';
-
-const Index = () => {
-  return <Text>Index Page</Text>;
-};
-
-export default Index;
