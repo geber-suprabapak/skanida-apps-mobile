d77c96b68c2aac4d2826c6f9935a8b12b85bc9f5
Hafiz Rayan Kurniawan <fiz@hrk.my.id>
2025-04-21 13:29:19 +0700

    Add button and text components, implement them in HomeScreen; update theme and color scheme management

---
+++ app/ui/button.tsx
@@ -0,0 +1,23 @@
+// app/home.tsx
+import { Stack, useRouter } from "expo-router";
+import {  View } from "react-native";
+import { Button } from '~/components/ui/button';
+import { Text } from '~/components/ui/text';
+
+export default function HomeScreen() {
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <View className="flex-1 items-center justify-center space-y-3">
+        <Button>
+            <Text>Lmao</Text>
+        </Button>
+      </View>
+    </>
+  );
+}
