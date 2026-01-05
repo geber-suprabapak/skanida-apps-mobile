37e6fcd12ca1b744c8d4ebdbed9d59da354a8c66
Hafiz Rayan Kurniawan <fiz@hrk.my.id>
2026-01-05 12:21:24 +0700

    chore(release): update version to 1.2.0-firefly

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Skanida Apps",
   slug: "skanida-apps-mobile",
-  version: "1.1.1-cyrene",
+  version: "1.2.0-firefly",
   runtimeVersion: {
     policy: "appVersion",
   },
